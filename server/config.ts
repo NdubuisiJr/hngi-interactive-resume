ee3f66a09a97efe65acf2ce58764a62692d8e3f3
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-08-19 01:07:37 +0100

    added mail sending code

---
+++ server/config.ts
@@ -0,0 +1,11 @@
+interface mailData{
+    email: string | undefined,
+    password: string | undefined
+}
+
+export const mailInfo: mailData = {
+    email: process.env.EMAIL,
+    password: process.env.PASSWORD
+};
+
+export const PORT: string|undefined = process.env.PORT;
---
+++ server/sendEmail.ts
@@ -0,0 +1,31 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import { mailInfo } from './config';
+
+
+const transporter: Transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: mailInfo.email,
+        pass: mailInfo.password
+    }
+});
+
+const sendEmail = (email: string, subject: string, message: string ) =>{
+    return new Promise((resolve, reject)=>{
+        transporter.sendMail({
+            from: mailInfo.email,
+            to: email,
+            subject: subject,
+            text: message
+        }, (err, info)=>{
+            if(err)
+                reject(err);
+            else
+                resolve(info);
+        });
+    });
+};
+
+export default sendEmail;
